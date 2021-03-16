27b95a4cfe4dab39e62b717a94e121335f189b76
solange-quispe <sol.kiss92@gmail.com>
2021-03-16 10:00:23 -0300

    Mis Practicas en JavasCript de Educacion IT

---
+++ Clase1.js
@@ -0,0 +1,6 @@
+// Un lenguaje de programacion es la manera en la cual nosotros escribimos de diferentes sintaxis, dependiendo con que lenguaje estemos escribiendo para indicarle a la computadora pasarle como una serie de pasos o instrucciones a traves de diferentes lenguajes porque hay muchos y la computadora entiende, interpreta nuestro lenguaje y ejecuta alguna instruccion, algun programa 
+// javascrip es un lenguaje de programacion del lado del cliente, del lado del navegador y sin embargo tambien trae su otro lado, esto quiere decir que los navegadores son los que interpretan el lenguaje y devuelven lo que nosotros le estemos pidiendo en las instrucciones 
+// ALGORITMO; es una serie de pasos que me va a permitir hacer una suma por ejemplo 
+// el algoritmo esta hecho con pseudocodigos.Esto es una forma de mostrar y que sea entendible para todo el mundo  
+// Esta seria de manera externa, y le pongo.JS
+// Los nombres de variables pueden contener letras, numeros, guion bajo y signo de dolar $
